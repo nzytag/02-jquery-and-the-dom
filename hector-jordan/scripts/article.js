fb262900fb9e7d30d9be6c285c581fe61212575e
Hector <norzagaray@me.com>
2017-11-29 11:28:41 -0800

    now js works

--- hector-jordan/scripts/article.js
+++ hector-jordan/scripts/article.js
@@ -23,15 +23,13 @@ Article.prototype.toHtml = function() {
   let $newArticle = $('article.template').clone();
   /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
 
-<<<<<<< HEAD
-=======
+
   $newArticle.removeClass('template').addClass('article');
->>>>>>> 2bacefd81c17952bea92cbec4d5e75d73cb88da4
 
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.attr('data-category', this.category);
 
-  /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
+  /* DONE: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
     We need to fill in:
       1. author name,
       2. author url,
@@ -39,11 +37,6 @@ Article.prototype.toHtml = function() {
       4. article body, and
       5. publication date. */
 
-<<<<<<< HEAD
-      
-
-  // REVIEW: Display the date as a relative number of 'days ago'
-=======
   $newArticle.find('a').text(this.author);
   $newArticle.find('a').attr('href', this.authorUrl);
   $newArticle.find('h1').text(this.title);
@@ -51,7 +44,6 @@ Article.prototype.toHtml = function() {
   $newArticle.find('time').text(this.publishedOn);
 
   // REVIEWED: Display the date as a relative number of 'days ago'
->>>>>>> 2bacefd81c17952bea92cbec4d5e75d73cb88da4
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
   $newArticle.append('<hr>');
   return $newArticle;
@@ -62,12 +54,22 @@ rawData.sort(function(a,b) {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
 
-// TODO: Refactor these for loops using the .forEach() array method.
+// DONE: Refactor these for loops using the .forEach() array method.
 
-for(let i = 0; i < rawData.length; i++) {
-  articles.push(new Article(rawData[i]));
-}
+// for(let i = 0; i < rawData.length; i++) {
+//   articles.push(new Article(rawData[i]));
+// }
 
-for(let i = 0; i < articles.length; i++) {
-  $('#articles').append(articles[i].toHtml());
-}
+rawData.forEach(function(data) {
+  articles.push(new Article(data))
+})
+
+
+
+// for(let i = 0; i < articles.length; i++) {
+//   $('#articles').append(articles[i].toHtml());
+// }
+
+articles.forEach(function(article) {
+  $('#articles').append(article.toHtml());
+})
